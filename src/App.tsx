9d600374c94dd696939b94c6518baf1b40b8d2ca
woo-sang-jeong <jeongwoosangdev@gmail.com>
2023-01-25 23:14:59 +0900

    create app, chakra ui, react routher dom install

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
+import { Text } from "@chakra-ui/react";
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function APP() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Text color={"red.500"} fontSize={"6xl"}> it's work! </Text>
     </div>
-  );
+  )
 }
 
-export default App;
+export default APP;
