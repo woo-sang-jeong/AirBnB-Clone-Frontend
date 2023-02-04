8f83948b74a8985bf5736b7ee9ed87e6e0df0367
woo-sang-jeong <jeongwoosangdev@gmail.com>
2023-02-05 06:48:05 +0900

    emails

--- src/routes/GithubConfirm.tsx
+++ src/routes/GithubConfirm.tsx
@@ -1,15 +1,29 @@
-import { Heading, Spinner, Text, VStack } from "@chakra-ui/react";
+import { Heading, Spinner, Text, useToast, VStack } from "@chakra-ui/react";
+import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { githubLogIn } from "../api";
 
 export default function GithubConfirm() {
   const { search } = useLocation();
+  const toast = useToast();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
   const confirmLogin = async () => {
     const params = new URLSearchParams(search);
     const code = params.get("code");
     if (code) {
-      await githubLogIn(code);
+      const status = await githubLogIn(code);
+      if (status === 200) {
+        toast({
+          status: "success",
+          title: "Welcome!",
+          position: "bottom-right",
+          description: "Happy to have you back!",
+        });
+        queryClient.refetchQueries(["me"]);
+        navigate("/");
+      }
     }
   };
   useEffect(() => {
@@ -17,7 +31,7 @@ export default function GithubConfirm() {
   }, []);
   return (
     <VStack justifyContent={"center"} mt={40}>
-      <Heading>Confirming, Log In...</Heading>
+      <Heading>Processing log in...</Heading>
       <Text>Don't go anywhere.</Text>
       <Spinner size="lg" />
     </VStack>
